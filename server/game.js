3ecda465cd4d3d146fdecddd35bc2121afb76529
Marcelo Miranda Carneiro <mcarneiro@fbiz.com.br>
2021-01-07 21:09:54 -0300

    better shuffle and event cards logic (wip)

--- server/game.js
+++ server/game.js
@@ -1,6 +1,18 @@
 const data = require('./data/core')
 const { generateID, clone } = require('./utils')
-const shuffle = arr => arr.concat().sort((a,b) => Math.random() >= 0.5 ? 1 : -1)
+const shuffle = arr => {
+  let len = arr.length
+  let newArr = arr.concat()
+
+  for (let i=0; i<len; i++) {
+    let j = Math.min(Math.floor(Math.random() * ((len) - (i+1))) + (i+1), len-1)
+    let oldVal = newArr[i]
+    newArr[i] = newArr[j]
+    newArr[j] = oldVal
+  }
+
+  return newArr
+}
 
 const getCharactersBy = list => (len) => {
   let characterList = list.concat()
@@ -60,6 +72,7 @@ const gameSetupBy = data => (userList) => {
     characterList: newCharacterList,
     enemyList: newEnemyList,
     eventList: newEventList,
+    eventIndex: 0,
     event: false,
     iconList,
     roundData: {},
@@ -75,22 +88,28 @@ const gameSetup = gameSetupBy(data)
 
 const newRound = (gameData) => {
 
+  // one round apply the actions and the other will apply event effects / or will be canceled
   if (gameData.event) {
-    if (gameData.event.cancelable && gameData.useToken && gameData.hasToken) {
-      let newGameData = clone(gameData)
+    let useToken = gameData.hasToken && Object.keys(gameData.roundData)
+      .filter(label => !gameData.roundData[label].useToken).length === 0
+    let newGameData = clone(gameData)
+
+    newGameData.useToken = useToken
+
+    if (newGameData.event.cancelable && newGameData.useToken && newGameData.hasToken) {
       newGameData.hasToken = false
       newGameData.useToken = false
       newGameData.event = null
       return newGameData
     }
 
-    if (!gameData.event.cancelable || !gameData.hasToken) {
-      gameData.event = null
-    }
+    newGameData.enemyList = applyEvent(newGameData.event.effect, newGameData.enemyList)
+    newGameData.event = null
 
-    // apply changes
+    return newGameData
   }
 
+  let tokenCount = 0
   let newGameData = Object.keys(gameData.roundData)
     .reduce((gameData, label) => {
       let roundData = gameData.roundData[label]
@@ -120,6 +139,12 @@ const newRound = (gameData) => {
         }
       })
 
+      let {toToken} = gameData.characterList.filter(({name}) => name === label)[0]
+
+      if (roundData.getToken) {
+        tokenCount += toToken
+      }
+
       return gameData
     }, clone(gameData))
 
@@ -132,6 +157,8 @@ const newRound = (gameData) => {
       }
     }
   })
+
+  newGameData.hasToken = tokenCount >= 1
 
   let timeIsUp = newGameData.enemyList.map(enemy => enemy.rounds.amount === 0).indexOf(true) >= 0
   let allEnemiesDestroyed = newGameData.enemyList.map(enemy => enemy.destroyed).indexOf(false) < 0
@@ -148,15 +175,55 @@ const newRound = (gameData) => {
 
   newGameData.timeBonus = newGameData.enemyList.reduce((acc, curr) => acc + (curr.destroyed ? curr.rounds.amount : 0), 0)
 
-  newGameData.event = newGameData.eventList.pop()
-
-  if (!newGameData.event.cancelable || !newGameData.hasToken) {
-    // apply changes
+  newGameData.event = newGameData.eventList[newGameData.eventIndex]
+  newGameData.eventIndex += 1
+  if (newGameData.eventIndex >= newGameData.eventList.length) {
+    newGameData.eventIndex = 0
   }
 
   newGameData.roundData = {}
   newGameData.round += 1
   return newGameData
+}
+
+const applyEvent = (effect, enemyList) => {
+  const shuffle = Math.floor(Math.random() * enemyList.length)
+  switch (effect.type) {
+    case 'resistance':
+      return enemyList.map((enemy, i) => {
+        if (i !== shuffle) {
+          return enemy
+        }
+
+        let resistanceEffect = effect.data.resistance.reduce((acc, val) => {
+          acc[val.label] = val.amount
+          return acc
+        }, {})
+
+        let resistance = enemy.resistance.map(resistance => {
+          let amount = Math.max(resistance.amount + (resistanceEffect[resistance.label] || 0), 0)
+
+          return {
+            ...resistance,
+            amount,
+            destroyed: amount === 0
+          }
+        })
+
+        let destroyed = enemy.resistance.filter(({destroyed}) => destroyed === false).length === 0
+
+        return {
+          ...enemy,
+          resistance,
+          destroyed
+        }
+      })
+    case 'block':
+      return enemyList
+    case 'enemy':
+      return enemyList
+    }
+  return enemyList
 }
 
 module.exports = {
--- server/game.test.js
+++ server/game.test.js
@@ -1,4 +1,4 @@
-const {gameSetupBy} = require('./game')
+const {gameSetupBy, newRound} = require('./game')
 
 test('setup with more tasks then cards will reuse multiplier list', () => {
   const userList = [{userName: 'a'}]
@@ -35,3 +35,412 @@ test('setup with more tasks then cards will reuse multiplier list', () => {
   expect(game.enemyList[0].enemyID).not.toBeNull()
   expect(game.enemyList[0].resistance[0].resistanceID).not.toBeNull()
 })
+
+test('user is added to character list of an enemy', () => {
+  const userList = [{userName: 'a'}]
+  const gameSetup = gameSetupBy({
+    "characterList": [{}],
+    "eventList": [{}],
+    "enemyList": [{
+      "rounds": {
+        "amount": 1
+      },
+      "resistance": [
+        {
+          "label": 'r1',
+          "amount": -1
+        }
+      ]
+    }],
+    "multiplierList": [ 1 ]
+  })
+  const game = gameSetup(userList)
+  const {enemyID} = game.enemyList[0]
+
+  game.roundData = {
+    "a": {
+      "resistanceID": [],
+      "enemyID": [enemyID]
+    }
+  }
+
+  const round = newRound(game)
+
+  expect(round.enemyList[0].characterList[0]).toBe('a')
+})
+
+test('game over - all enemies destroyed', () => {
+  const userList = [{userName: 'a'}]
+  const gameSetup = gameSetupBy({
+    "characterList": [{}],
+    "eventList": [{}],
+    "enemyList": [{
+      "rounds": {
+        "amount": 1
+      },
+      "resistance": [
+        {
+          "label": 'r1',
+          "amount": -1
+        }
+      ]
+    }],
+    "multiplierList": [ 1 ]
+  })
+  const game = gameSetup(userList)
+  const {resistanceID} = game.enemyList[0].resistance[0]
+
+  game.roundData = {
+    "a": {
+      "resistanceID": [resistanceID],
+      "enemyID": []
+    }
+  }
+
+  const round = newRound(game)
+  expect(round.status).toBe('end')
+  expect(round.statusMessage).toBe('win')
+})
+
+test('game over - time is up', () => {
+  const userList = [{userName: 'a'}]
+  const gameSetup = gameSetupBy({
+    "characterList": [{}],
+    "eventList": [{}],
+    "enemyList": [{
+      "rounds": {
+        "amount": 1
+      },
+      "resistance": [
+        {
+          "label": 'r1',
+          "amount": -1
+        }
+      ]
+    }],
+    "multiplierList": [ 2 ]
+  })
+  const game = gameSetup(userList)
+  const resistanceID = game.enemyList[0].resistance[0].resistanceID
+
+  game.roundData = {
+    "a": {
+      "resistanceID": [resistanceID],
+      "enemyID": []
+    }
+  }
+
+  const round = newRound(game)
+  expect(round.status).toBe('end')
+  expect(round.statusMessage).toBe('time is up')
+
+})
+
+test('events are shuffled, applied', () => {
+  const userList = [{userName: 'a'}]
+  const gameSetup = gameSetupBy({
+    "characterList": [{
+      "skill": [ "plus", "star"],
+      "toToken": 1
+    }],
+    "eventList": [
+      {
+        "cancelable": true,
+        "effect": {
+          "type": "resistance",
+          "data": {
+            "resistance": [
+              {
+                "label": "r1",
+                "amount": 1
+              }
+            ]
+          }
+        }
+      },
+      {
+        "cancelable": true,
+        "effect": {
+          "type": "resistance",
+          "data": {
+            "resistance": [
+              {
+                "label": "r1",
+                "amount": 2
+              }
+            ]
+          }
+        }
+      },
+      {
+        "cancelable": true,
+        "effect": {
+          "type": "resistance",
+          "data": {
+            "resistance": [
+              {
+                "label": "r1",
+                "amount": 3
+              }
+            ]
+          }
+        }
+      }
+    ],
+    "enemyList": [{
+      "rounds": {
+        "amount": 10
+      },
+      "resistance": [
+        {
+          "label": 'r1',
+          "amount": 2
+        }
+      ]
+    },{
+      "rounds": {
+        "amount": 10
+      },
+      "resistance": [
+        {
+          "label": 'r1',
+          "amount": 2
+        }
+      ]
+    }],
+    "multiplierList": [ 2 ]
+  })
+
+  let round = gameSetup(userList)
+
+  expect(round.eventList.map(event => event.effect.data.resistance[0].amount).join(',')).not.toBe('1,2,3')
+  round = newRound(round)
+  round = newRound(round)
+
+  expect(round.enemyList[0].resistance[0]).not.toBe(2)
+})
+
+test('when there are no more events they will restart', () => {
+  const userList = [{userName: 'a'}]
+  const gameSetup = gameSetupBy({
+    "characterList": [{
+      "skill": [ "plus", "star"],
+      "toToken": 1
+    }],
+    "eventList": [
+      {
+        "cancelable": true,
+        "effect": {"type": ""}
+      },
+      {
+        "cancelable": true,
+        "effect": {"type": ""}
+      }
+    ],
+    "enemyList": [{
+      "rounds": {
+        "amount": 10
+      },
+      "resistance": [
+        {
+          "label": 'r1',
+          "amount": 2
+        }
+      ]
+    }],
+    "multiplierList": [ 2 ]
+  })
+
+  let round = gameSetup(userList)
+  round = newRound(round)
+  expect(round.eventIndex).toBe(1)
+  round = newRound(round)
+  round = newRound(round)
+  expect(round.eventIndex).toBe(0)
+
+})
+
+test('new event (use token)', () => {
+  const userList = [{userName: 'a'}]
+  const gameSetup = gameSetupBy({
+    "characterList": [
+      {
+        "skill": [ "plus", "star"],
+        "toToken": 1
+      }
+    ],
+    "eventList": [
+      {
+      "cancelable": true,
+      }
+    ],
+    "enemyList": [{
+      "rounds": {
+        "amount": 10
+      },
+      "resistance": [
+        {
+          "label": 'r1',
+          "amount": -1
+        }
+      ]
+    }],
+    "multiplierList": [ 2 ]
+  })
+  let round = gameSetup(userList)
+
+  round.roundData = {
+    "a": {
+      "resistanceID": [],
+      "enemyID": [],
+      "getToken": true
+    }
+  }
+
+  round = newRound(round)
+
+  round.roundData = {
+    "a": {
+      "resistanceID": [],
+      "enemyID": [],
+      "useToken": true
+    }
+  }
+
+  round = newRound(round)
+
+  expect(round.event).toBeNull()
+  expect(round.enemyList[0].resistance[0].amount).toBe(2)
+})
+
+test('new event (type resistance positive)', () => {
+  const userList = [{userName: 'a'}]
+  const gameSetup = gameSetupBy({
+    "characterList": [{
+      "skill": [ "plus", "star"],
+      "toToken": 1
+    }],
+    "eventList": [
+      {
+        "cancelable": true,
+        "effect": {
+          "type": "resistance",
+          "data": {
+            "resistance": [
+              {
+                "label": "r1",
+                "amount": 2
+              }
+            ]
+          }
+        }
+      }
+    ],
+    "enemyList": [{
+      "rounds": {
+        "amount": 10
+      },
+      "resistance": [
+        {
+          "label": 'r1',
+          "amount": -1
+        }
+      ]
+    }],
+    "multiplierList": [ 2 ]
+  })
+  let round = gameSetup(userList)
+
+  round = newRound(round)
+  round = newRound(round)
+
+  expect(round.enemyList[0].resistance[0].amount).toBe(4)
+})
+
+test('new event (type resistance negative)', () => {
+  const userList = [{userName: 'a'}]
+  const gameSetup = gameSetupBy({
+    "characterList": [
+      {
+        "skill": [ "plus", "star"],
+        "toToken": 1
+      }
+    ],
+    "eventList": [
+      {
+        "cancelable": true,
+        "effect": {
+          "type": "resistance",
+          "data": {
+            "resistance": [
+              {
+                "label": "r1",
+                "amount": -2
+              }
+            ]
+          }
+        }
+      }
+    ],
+    "enemyList": [
+      {
+        "rounds": {
+          "amount": 10
+        },
+        "resistance": [
+          {
+            "label": 'r1',
+            "amount": -1
+          }
+        ]
+      }
+    ],
+    "multiplierList": [ 2 ]
+  })
+  let round = gameSetup(userList)
+
+  round = newRound(round)
+  round = newRound(round)
+
+  expect(round.enemyList[0].resistance[0].amount).toBe(0)
+  expect(round.enemyList[0].resistance[0].destroyed).toBe(true)
+  expect(round.enemyList[0].destroyed).toBe(true)
+})
+
+test('new event (type block)', () => {
+})
+test('new event (type new enemy)', () => {
+})
+
+
+// "effect": {
+//   "type": "enemy",
+//   "cancelable": false,
+//   "data": {
+//     "resistance": [
+//       {
+//         "label": "circle",
+//         "amount": 2
+//       }, {
+//         "label": "drop",
+//         "amount": 2
+//       }
+//     ],
+//     "rounds": 6
+//   }
+
+
+// {
+//   "effect": {
+//     "type": "resistance",
+//     "cancelable": true,
+//     "data": {
+//       "resistance": [
+//         {
+//           "label": "plus",
+//           "amount": 2
+//         }
+//       ]
+//     }
+//   }
+// }
